247c66d6cfe5d82fcf12c247f43c2aee8143e55a
jaehyunlee <jaehyunlee25@gmail.com>
2021-10-12 18:23:28 +0900

    renew

--- pages/api/post/hello.js
+++ pages/api/post/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  // #1. cors 해제
+  res.writeHead(200, {
+    'Access-Control-Allow-Origin': '*', // for same origin policy
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Headers': ['Content-Type', 'Authorization'], // for application/json
+    'Access-Control-Allow-Methods': 'GET',
+  });
   res.status(200).json({ name: 'John Doe' });
 }
